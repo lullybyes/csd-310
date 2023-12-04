a0dad8bc3935faed219d5377a91faf43f9988fd0
zzzsupernova <LYin13@gmail.com>
2023-12-03 17:44:45 -0800

    Committing CSD 310 group project

---
+++ module_10/bacchus_db_init.sql
@@ -0,0 +1,229 @@
+/*
+    Title: bacchus_db_init.sql
+    Author: Brandon Hackett, Darnell Lewis, Derek L
+    Date: 12/2/23
+    Description: bacchus database initialization script.
+*/
+
+-- drop database user if exists 
+DROP USER IF EXISTS 'bacchus_user'@'localhost';
+
+
+-- create movies_user and grant them all privileges to the movies database 
+CREATE USER 'bacchus_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ILoveWine!';
+
+-- grant all privileges to the movies database to user movies_user on localhost 
+GRANT ALL PRIVILEGES ON bacchus.* TO 'bacchus_user'@'localhost';
+
+
+-- drop tables if they are present
+DROP TABLE IF EXISTS employee_clock;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS supply_order_line;
+DROP TABLE IF EXISTS supply_order;
+DROP TABLE IF EXISTS supplier;
+DROP TABLE IF EXISTS component;
+DROP TABLE IF EXISTS wine_order_line;
+DROP TABLE IF EXISTS wine_order;
+DROP TABLE IF EXISTS wine;
+DROP TABLE IF EXISTS distributor;
+
+-- create the employee table 
+CREATE TABLE employee (
+    emp_id     INT             NOT NULL        AUTO_INCREMENT,
+    emp_first   VARCHAR(50)     NOT NULL,
+    emp_last   VARCHAR(50)     NOT NULL,
+    emp_position   VARCHAR(50)     NOT NULL,
+    emp_rate DECIMAL(6,2) NOT NULL,
+    emp_hire_date   DATE           NOT NULL,
+
+    PRIMARY KEY(emp_id)
+); 
+
+-- create the employee_clock table 
+CREATE TABLE employee_clock (
+    clock_id     INT             NOT NULL        AUTO_INCREMENT,
+    emp_id   INT     NOT NULL,
+    clock_in  DATETIME   NOT NULL,
+    clock_out DATETIME, 
+    PRIMARY KEY(clock_id),
+    CONSTRAINT fk_employee
+    FOREIGN KEY(emp_id)
+        REFERENCES employee(emp_id)
+); 
+-- create the supplier table
+CREATE TABLE supplier (
+    supplier_id   INT             NOT NULL        AUTO_INCREMENT,
+    supplier_name  VARCHAR(75)     NOT NULL,
+    supplier_contact   VARCHAR(75),
+	supplier_email VARCHAR(75),
+	supplier_phone VARCHAR(15) NOT NULL,
+    PRIMARY KEY(supplier_id)
+);
+-- create component table
+CREATE TABLE component (
+    component_id   INT             NOT NULL        AUTO_INCREMENT,
+    component_name  VARCHAR(75)     NOT NULL,
+    component_inv_qty INT NOT NULL,
+    PRIMARY KEY(component_id)
+);
+
+-- create supply_order table
+CREATE TABLE supply_order (
+    supply_order_id   INT             NOT NULL        AUTO_INCREMENT,
+    supplier_id  INT     NOT NULL,
+    order_date DATE NOT NULL,
+    expected_delivery DATE,
+    actual_delivery DATE,
+    PRIMARY KEY (supply_order_id),
+    CONSTRAINT fk_supplier
+    FOREIGN KEY(supplier_id)
+        REFERENCES supplier(supplier_id)
+);
+
+CREATE TABLE supply_order_line (
+    supply_order_line_id   INT             NOT NULL        AUTO_INCREMENT,
+    supply_order_id  INT     NOT NULL,
+    component_id INT NOT NULL,
+    component_quantity INT NOT NULL,
+    component_price DECIMAL(8,2) NOT NULL,
+    PRIMARY KEY (supply_order_line_id),
+    CONSTRAINT fk_order_id
+    FOREIGN KEY(supply_order_id)
+        REFERENCES supply_order(supply_order_id),
+    CONSTRAINT fk_component_id
+    FOREIGN KEY(component_id)
+        REFERENCES component(component_id)
+);
+
+CREATE TABLE distributor (
+    distributor_id   INT             NOT NULL        AUTO_INCREMENT,
+    distributor_name  VARCHAR(75)     NOT NULL,
+    distributor_contact   VARCHAR(75),
+	distributor_email VARCHAR(75),
+	distributor_phone VARCHAR(15) NOT NULL,
+    PRIMARY KEY(distributor_id)
+);
+-- create wine table
+CREATE TABLE wine (
+    wine_id   INT             NOT NULL        AUTO_INCREMENT,
+    wine_type  VARCHAR(75)     NOT NULL,
+    wine_production_date DATE NOT NULL,
+    wine_qty_produced INT NOT NULL,
+    PRIMARY KEY(wine_id)
+);
+
+-- create wine_order table
+CREATE TABLE wine_order (
+    wine_order_id   INT    NOT NULL        AUTO_INCREMENT,
+    distributor_id  INT     NOT NULL,
+    order_date DATE NOT NULL,
+    expected_delivery DATE,
+    actual_delivery DATE,
+    PRIMARY KEY (wine_order_id),
+    CONSTRAINT fk_distributor
+    FOREIGN KEY(distributor_id)
+        REFERENCES distributor(distributor_id)
+);
+
+CREATE TABLE wine_order_line (
+    wine_order_line_id   INT             NOT NULL        AUTO_INCREMENT,
+    wine_order_id  INT     NOT NULL,
+    wine_id INT NOT NULL,
+    wine_quantity INT NOT NULL,
+    wine_price DECIMAL(8,2) NOT NULL,
+    PRIMARY KEY (wine_order_line_id),
+    CONSTRAINT fk_wine_order_id
+    FOREIGN KEY(wine_order_id)
+        REFERENCES wine_order(wine_order_id),
+    CONSTRAINT fk_wine_id
+    FOREIGN KEY(wine_id)
+        REFERENCES wine(wine_id)
+);
+
+INSERT INTO employee
+    VALUES
+    (NULL, "Debrah", "Messing","Production", 18.00,'2020-03-20'),
+    (NULL, "Brad", "Pitt","Shift Lead", 20.00,'2018-08-21'),
+    (NULL, "Tina", "Fey","Custodian", 17.00,'2019-04-07'),
+    (NULL, "Debbie", "Reynolds","Production", 18.00,'2021-03-02'),
+    (NULL, "Alec", "Baldwin","Production", 18.00,'2022-05-28'),
+    (NULL, "Amy", "Poehler","Quality Assurance", 23.00,'2022-05-28');
+
+INSERT INTO employee_clock
+    VALUES
+    (NULL, 1,'2023-11-27 08:00','2023-11-27 16:00'),
+    (NULL, 2,'2023-11-27 07:30','2023-11-27 15:30'),
+    (NULL, 3,'2023-11-27 12:00','2023-11-27 20:00'),
+    (NULL, 4,'2023-11-27 08:00','2023-11-27 16:00'),
+    (NULL, 5,'2023-11-27 08:00','2023-11-27 16:00'),
+    (NULL, 6,'2023-11-27 09:00','2023-11-27 17:00');
+
+INSERT INTO supplier
+    VALUES
+    (NULL, "Wine Time","Joe Smith", "joesmith@winetime.com", "8003224567"),
+    (NULL, "Boxes and Things","Mary Johnson", "mjohnson@boxesnthings.com", "8004567892"),
+    (NULL, "Brewing Buds","Jeff Field", "jeff.field@brewingbuds.com", "6053725555");
+
+INSERT INTO supply_order
+    VALUES
+    (NULL, 1, '2023-11-01','2023-11-08','2023-11-09'),
+    (NULL, 1, '2023-11-05','2023-11-12','2023-11-13'),
+    (NULL, 2, '2023-11-02','2023-11-06','2023-11-06'),
+    (NULL, 2, '2023-11-10','2023-11-16','2023-11-15'),
+    (NULL, 3, '2023-11-03','2023-11-06','2023-11-07'),
+    (NULL, 3, '2023-11-28','2023-12-02', NULL);
+
+INSERT INTO component
+    VALUES
+    (NULL, "750ml Glass Bottle", 200),
+    (NULL, "Standard Cork", 500),
+    (NULL, "Labels", 1000),
+    (NULL, "12-bottle Cardboard Box", 100),
+    (NULL, "225 Liter Vat ", 10),
+    (NULL, "Vinyl Tubing Ft.", 300);
+
+INSERT INTO supply_order_line
+    VALUES
+    (NULL, 1, 1, 300, 0.98),
+    (NULL, 2, 2, 200, 0.38),
+    (NULL, 3, 3, 500, 0.22),
+    (NULL, 4, 4, 100, 1.25),
+    (NULL, 5, 5, 20, 638),
+    (NULL, 6, 6, 200, 1.20);
+
+INSERT INTO wine
+    VALUES
+    (NULL, "Chardonnay", '2023-09-15', 300),
+    (NULL, "Chablis", '2023-08-15', 300),
+    (NULL, "Merlot", '2023-08-30', 300),
+    (NULL, "Cabernet", '2023-09-05', 300),
+    (NULL, "Red Blend", '2023-08-20', 300),
+    (NULL, "White Blend", '2023-10-01', 300);
+
+INSERT INTO distributor
+    VALUES
+    (NULL, "Ben's Distribution Co", "Jeff Stevens", "jstevens@bensdistco.com", "6884598764"),
+    (NULL, "Larry's Liquor Store", "Larry Jones", NULL, "6235761234"),
+    (NULL, "Betty's Wine and Spirits Wholesale", "Jessica Lee", "jlee@bettyswinenspirits.com","6665551234"),
+    (NULL, "Cask and Cork Wine Distribution", "Sam Rogers", "srogers@caskncork.com","3654511572"),
+    (NULL, "Wine World", "Jeff Goldblum", "jeff.goldblum@wineworld.com","8887776543"),
+    (NULL, "In Vino Veritas", "Vicki Vivacious", "v.vivacious@invinoveritas.com","6055558888");
+
+INSERT INTO wine_order
+    VALUES
+    (NULL, 1, '2023-11-02','2023-11-09','2023-11-10'),
+    (NULL, 2, '2023-11-04','2023-11-11','2023-11-12'),
+    (NULL, 3, '2023-11-04','2023-11-08','2023-11-08'),
+    (NULL, 4, '2023-11-11','2023-11-17','2023-11-17'),
+    (NULL, 5, '2023-11-02','2023-11-07','2023-11-07'),
+    (NULL, 6, '2023-11-30','2023-12-03', NULL);
+
+INSERT INTO wine_order_line
+    VALUES
+    (NULL, 1, 1, 120, 24.50),
+    (NULL, 2, 2, 24, 24.50),
+    (NULL, 3, 3, 60, 24.50),
+    (NULL, 4, 4, 100, 24.50),
+    (NULL, 5, 5, 72, 24.50),
+    (NULL, 6, 6, 96, 24.50);
---
+++ module_10/bacchus_tables.py
@@ -0,0 +1,105 @@
+# Group 5
+# Display Tables and Labels
+
+# Couldn't write a method that allowed for the granularity of good label names so we followed
+# the norm of performing a query, fetching the results, printing a title line,
+# then iterating through the entries and putting them with proper labels.
+
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+    "user":"bacchus_user",
+    "password":"ILoveWine!",
+    "host":"127.0.0.1",
+    "database":"bacchus",
+    "raise_on_warnings": True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("  The supplied username or password are invalid")
+    
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("  The specified database does not exist")
+    
+    else:
+        print(err)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM employee")
+employees = cursor.fetchall()
+print("--DISPLAYING Employee RECORDS --")
+for employee in employees:
+    print("Employee ID: {}\nEmployee Name: {} {}\nEmployee Position: {}\nEmployee Pay Rate: ${}\nEmployee Hire Date: {}\n".format(employee[0],employee[1],employee[2],employee[3],employee[4],employee[5]))
+
+cursor.execute("SELECT clock_id, emp_first, emp_last, clock_in, clock_out "
+               "FROM employee INNER JOIN employee_clock on employee_clock.emp_id=employee.emp_id")
+clockrecords = cursor.fetchall()
+print("--DISPLAYING Clock-in RECORDS --")
+for clockrecord in clockrecords:
+    print("Clock-in ID: {}\nEmployee Name: {} {}\nClock In: {}\nClock Out: {}\n".format(clockrecord[0],clockrecord[1],clockrecord[2],clockrecord[3],clockrecord[4]))
+
+cursor.execute("SELECT * FROM distributor")
+distributors = cursor.fetchall()
+print("--DISPLAYING Distributor RECORDS --")
+for distributor in distributors:
+    print("Distributor ID: {}\nDistributor Name: {}\nDistributor Contact: {}\nDistributor Email: {}\nDistributor Phone: {}\n".format(distributor[0],distributor[1],distributor[2],distributor[3],distributor[4]))
+
+cursor.execute("SELECT * FROM wine")
+wines = cursor.fetchall()
+print("--DISPLAYING Wine RECORDS --")
+for wine in wines:
+    print("Wine ID: {}\nWine Type: {}\nWine Production Date: {}\nWine Quantity (Bottles): {}\n".format(wine[0],wine[1],wine[2],wine[3]))
+
+cursor.execute("SELECT wine_order_id, distributor_name,order_date,expected_delivery,actual_delivery "
+               "FROM wine_order INNER JOIN distributor on distributor.distributor_id=wine_order.distributor_id")
+wineorders = cursor.fetchall()
+print("--DISPLAYING Wine Order RECORDS --")
+for wineorder in wineorders:
+    print("Wine Order ID: {}\nDistributor Name: {}\nOrder Date: {}\nExpected Delivery: {}\nActual Delivery: {}\n".format(wineorder[0],wineorder[1],wineorder[2],wineorder[3],wineorder[4]))
+
+cursor.execute("SELECT wine_order_line_id,distributor_name,wine_type,wine_quantity, wine_price "
+                "FROM wine_order INNER JOIN distributor on distributor.distributor_id=wine_order.distributor_id "
+                "INNER JOIN wine_order_line on wine_order.wine_order_id=wine_order_line.wine_order_id "
+		        "INNER JOIN wine on wine.wine_id=wine_order_line.wine_id;")
+wineorderlines = cursor.fetchall()
+print("--DISPLAYING Wine Order Line RECORDS --")
+for wineorderline in wineorderlines:
+    print("Wine Order Line ID: {}\nDistributor Name: {}\nWine Type: {}\nWine Quantity: {}\nWine Price: ${}\n".format(wineorderline[0],wineorderline[1],wineorderline[2],wineorderline[3],wineorderline[4]))
+
+cursor.execute("SELECT * FROM supplier")
+suppliers = cursor.fetchall()
+print("--DISPLAYING Supplier RECORDS --")
+for supplier in suppliers:
+    print("Supplier ID: {}\nSupplier Name: {}\nSupplier Contact: {}\nSupplier Email: {}\nSupplier Phone: {}\n".format(supplier[0],supplier[1],supplier[2],supplier[3],supplier[4]))
+
+cursor.execute("SELECT * FROM component")
+components = cursor.fetchall()
+print("--DISPLAYING Component RECORDS --")
+for component in components:
+    print("Component ID: {}\ncomponent Name: {}\nComponent Quantity On-hand: {}\n".format(component[0],component[1],component[2]))
+
+cursor.execute("SELECT supply_order_id, supplier_name,order_date,expected_delivery,actual_delivery "
+               "FROM supply_order INNER JOIN supplier on supplier.supplier_id=supply_order.supplier_id")
+supplyorders = cursor.fetchall()
+print("--DISPLAYING Supply Order RECORDS --")
+for supplyorder in supplyorders:
+    print("Supply Order ID: {}\nSupplier Name: {}\nOrder Date: {}\nExpected Delivery: {}\nActual Delivery: {}\n".format(supplyorder[0],supplyorder[1],supplyorder[2],supplyorder[3],supplyorder[4]))
+
+cursor.execute("SELECT supply_order_line_id,supplier_name,component_name,component_quantity,component_price "
+                "FROM supply_order INNER JOIN supplier on supplier.supplier_id=supply_order.supplier_id "
+                "INNER JOIN supply_order_line on supply_order.supply_order_id=supply_order_line.supply_order_id "
+		        "INNER JOIN component on component.component_id=supply_order_line.component_id;")
+supplyorderlines = cursor.fetchall()
+print("--DISPLAYING Supply Order Line RECORDS --")
+for supplyorderline in supplyorderlines:
+    print("Supply Order Line ID: {}\nSupplier Name: {}\nSupply Type: {}\nQuantity Ordered: {}\nSupply Price/Unit: ${}\n".format(supplyorderline[0],supplyorderline[1],supplyorderline[2],supplyorderline[3],supplyorderline[4]))
+
+input("Press Enter to Exit...")
+db.close()
